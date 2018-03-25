9f179c9b9d7e37cecf399d73bfa1bad03f7c7b18
Xinh Nguyen <nguyentrucxjnh@gmail.com>
2018-03-25 23:10:51 +0700

    :sparkles: Fix API update card

--- services/cards/updateCard.js
+++ services/cards/updateCard.js
@@ -4,6 +4,7 @@ const Card = mongoose.model('Card')
 const logger = require('../../helpers/loggerHelpers')
 
 exports.updateCard = async (id, data) => {
+  delete data._id
   await Card.findByIdAndUpdate(id, data)
   logger.info(`Card updated`)
 }
